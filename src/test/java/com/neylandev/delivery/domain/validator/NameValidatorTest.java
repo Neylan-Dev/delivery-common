4df9160bfcad7f9fffb6ffb6b3b231c5f5ac61d6
Neylan.Santos <neylan.santos@radixeng.com.br>
2022-07-27 12:15:02 -0300

    Initial commit

---
+++ src/test/java/com/neylandev/delivery/domain/validator/NameValidatorTest.java
@@ -0,0 +1,14 @@
+package com.neylandev.delivery.domain.validator;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@ExtendWith(MockitoExtension.class)
+class NameValidatorTest {
+
+    @Autowired
+    private NameValidator nameValidator;
+
+    
+}
