cbea9ba959fe601b128ced6b7c0e6c2b2854f858
Neylan-Body <neylan2011@gmail.com>
2022-07-31 18:22:22 -0300

    adição de DTO

---
+++ src/main/java/com/neylandev/delivery/domain/dto/DeliveryEmailDto.java
@@ -0,0 +1,17 @@
+package com.neylandev.delivery.domain.dto;
+
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DeliveryEmailDto {
+
+    private String clientName;
+    private String clientEmail;
+    private String subject;
+    private String body;
+
+}
