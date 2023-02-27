10866541eacbbd925b7227a9ba9e872a36f14098
Neylan-Body <neylan2011@gmail.com>
2023-02-27 20:32:11 -0300

    adicionando categorias em português

--- src/main/java/com/neylandev/delivery/domain/enums/Category.java
+++ src/main/java/com/neylandev/delivery/domain/enums/Category.java
@@ -3,18 +3,26 @@ package com.neylandev.delivery.domain.enums;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 @RequiredArgsConstructor
 @Getter
 public enum Category {
-    ELECTRONICS("Electronics"),
-    CLOTHING("Clothing"),
-    BOOKS("Books"),
-    BEAUTY("Beauty"),
-    FOOD("Food"),
-    SPORTS("Sports"),
-    HOME("Home"),
-    TOYS("Toys");
+    ELECTRONICS("Electronics", "Eletronicos"),
+    CLOTHING("Clothing", "Vestuário"),
+    BOOKS("Books","Livros"),
+    BEAUTY("Beauty", "Beleza"),
+    FOOD("Food", "Comida"),
+    SPORTS("Sports", "Esportes"),
+    HOME("Home", "Casa"),
+    TOYS("Toys", "Brinquedos");
 
     private final String name;
+
+    private final String portugueseName;
+
+    private final List<String> portugueseCategories = Stream.of(Category.values()).map(Category::getPortugueseName).collect(Collectors.toList());
 }
 
